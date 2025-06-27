92a263cd78439ef002be24274a2fd8e939ff7994
Krisno Mukti <krsnmvk@gmail.com>
2025-06-27 07:46:32 +0700

    03: trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import { caller } from '@/trpc/server';
+
+export default async function Home() {
+  const data = await caller.hello({ text: 'Krisno Mukti' });
+
   return (
     <div>
       <h1>Home Page</h1>
+
+      {JSON.stringify(data, null, 2)}
     </div>
   );
 }
