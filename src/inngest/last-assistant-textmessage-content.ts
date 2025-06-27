5be746ed7220a86fbdade59ccac6832bd38d2aff
Krisno Mukti <krsnmvk@gmail.com>
2025-06-27 17:54:27 +0700

    07: agent tools

---
+++ src/inngest/last-assistant-textmessage-content.ts
@@ -0,0 +1,17 @@
+import { AgentResult, TextMessage } from '@inngest/agent-kit';
+
+export function lastAssistantTextMessageContent(result: AgentResult) {
+  const lastAssistantTextMessageIndex = result.output.findLastIndex(
+    (message) => message.role === 'assistant'
+  );
+
+  const message = result.output[lastAssistantTextMessageIndex] as
+    | TextMessage
+    | undefined;
+
+  return message?.content
+    ? typeof message.content === 'string'
+      ? message.content
+      : message.content.map((c) => c.text).join('')
+    : undefined;
+}
